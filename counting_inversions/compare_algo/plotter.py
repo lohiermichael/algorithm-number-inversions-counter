f713fb584991b0140c63d6187925a78dc1d86ea7
michael <michael.lohier@cognite.com>
2019-08-31 14:06:56 +0200

    Revert "Create a simple plot function for ehe dataFrame with multiple outputs"
    This reverts commit c86a321bfeb1aa524483a8d8fbc9d0575ebdfcc2.

--- counting_inversions/compare_algo/plotter.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-
-def algo_plotter(df_comparison: pd.DataFrame):
-    fig, ax = plt.subplots(figsize=(10, 10))
-    df_comparison.plot(kind='line', ax=ax)
-    ax.set_xlabel(xlabel="List length")
-    ax.set_ylabel(ylabel="Time")
